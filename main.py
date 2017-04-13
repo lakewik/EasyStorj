faa0d1934808bef936b7c9020dcc5b73605c9dce
Marco Rosa <marcor165@hotmail.it>
2017-04-13 15:22:14 +0200

    fix password error in StorjEngine class

--- main.py
+++ main.py
@@ -396,6 +396,7 @@ class LoginUI(QtGui.QMainWindow):
 class StorjEngine():
     def __init__(self):
         account_manager = AccountManager()
+        self.password = None
         if account_manager.if_logged_in():
             self.password = account_manager.get_user_password()
             self.email = account_manager.get_user_email()
