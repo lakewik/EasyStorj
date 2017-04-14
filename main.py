f3cd946adbb50f66e48cb7389f556e1cfed35f3b
Wiktor Jezioro <wikjezioro@op.pl>
2017-04-14 11:01:07 +0200

    Remove unnecessary classes

--- main.py
+++ main.py
@@ -67,71 +67,9 @@ html_format_end = "</span></p></body></html>"
 
 
 
-class ProgressBar(QProgressBar):
-
-    def __init__(self, value, parent=None):
-        QProgressBar.__init__(self)
-        self.setMinimum(1)
-        self.setMaximum(100)
-        self.setValue(value)
-        self.setFormat('{0:.5f}'.format(value))
-        #style = ''' QProgressBar{max-height: 15px;text-align: center;}'''
-        #self.setStyleSheet(style)
-
-class ProgressWidgetItem(QTableWidgetItem):
-
-    def __lt__(self, other):
-        return self.data(Qt.UserRole) < other.data(Qt.UserRole)
-
-    def updateValue(self, value):
-        self.setData(Qt.UserRole, value)
-
-
-class MyTableModel(QtCore.QAbstractTableModel):
-    def __init__(self,data,parent=None):
-        QtCore.QAbstractTableModel.__init__(self,parent)
-        self.__data=data     # Initial Data
-
-    def rowCount( self, parent ):
-        return len(self.__data)
-
-    def columnCount( self , parent ):
-        return len(self.__data)
-
-    def data ( self , index , role ):
-        if role == QtCore.Qt.DisplayRole:
-            row = index.row()
-            column = index.column()
-            value = self.__data[row][column]
-            return QtCore.QString(str(value))
-
-    def setData(self, index, value):
-        self.__data[index.row()][index.column()] = value
-        return True
-
-    def flags(self, index):
-        return QtCore.Qt.ItemIsEnabled|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsSelectable
-
-    def insertRows(self , position , rows , item , parent=QtCore.QModelIndex()):
-        # beginInsertRows (self, QModelIndex parent, int first, int last)
-        self.beginInsertRows(QtCore.QModelIndex(),len(self.__data),len(self.__data)+1)
-        self.__data.append(item) # Item must be an array
-        self.endInsertRows()
-        return True
-
-
 class StorjSDKImplementationsOverrides():
     def __init__(self, parent=None):
         self.storj_engine = StorjEngine()  # init StorjEngine
-
-
-
-
-
-
-
-
-
 
 
 
