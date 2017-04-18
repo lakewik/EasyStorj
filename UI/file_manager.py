a7d1cc37bf94d596079ff49b7a265be22ceb3c3a
Wiktor Jezioro <wikjezioro@op.pl>
2017-04-18 15:52:47 +0200

    Add missing exception

--- UI/file_manager.py
+++ UI/file_manager.py
@@ -9,6 +9,7 @@ from file_download import SingleFileDownloadUI
 import storj.exception as sjexc
 import threading
 
+
 # Files section
 class FileManagerUI(QtGui.QMainWindow):
 
@@ -99,7 +100,7 @@ class FileManagerUI(QtGui.QMainWindow):
 
         self.tools = Tools()
 
-        model = QtGui.QStandardItemModel(1, 1)  # initialize model for inserting to table
+        model = QStandardItemModel(1, 1)  # initialize model for inserting to table
 
         model.setHorizontalHeaderLabels(['File name', 'File size', 'Mimetype', 'File ID'])
 
@@ -107,29 +108,29 @@ class FileManagerUI(QtGui.QMainWindow):
         self.current_selected_bucket_id = self.bucket_id_list[self.current_bucket_index]
 
         i = 0
+        try:
+            for self.file_details in self.storj_engine.storj_client.bucket_files(str(self.current_selected_bucket_id)):
+                item = QStandardItem(str(self.file_details["filename"]))
+                model.setItem(i, 0, item)  # row, column, item (QStandardItem)
 
-        for self.file_details in self.storj_engine.storj_client.bucket_files(str(self.current_selected_bucket_id)):
-            item = QtGui.QStandardItem(str(self.file_details["filename"]))
-            model.setItem(i, 0, item)  # row, column, item (StandardItem)
+                file_size_str = self.tools.human_size(int(self.file_details["size"]))  # get human readable file size
 
-            file_size_str = self.tools.human_size(int(self.file_details["size"]))  # get human readable file size
+                item = QStandardItem(str(file_size_str))
+                model.setItem(i, 1, item)  # row, column, item (QStandardItem)
 
-            item = QtGui.QStandardItem(str(file_size_str))
-            model.setItem(i, 1, item)  # row, column, item (QQtGui.StandardItem)
+                item = QStandardItem(str(self.file_details["mimetype"]))
+                model.setItem(i, 2, item)  # row, column, item (QStandardItem)
 
-            item = QtGui.QStandardItem(str(self.file_details["mimetype"]))
-            model.setItem(i, 2, item)  # row, column, item (QStandardItem)
+                item = QStandardItem(str(self.file_details["id"]))
+                model.setItem(i, 3, item)  # row, column, item (QStandardItem)
 
-            item = QtGui.QStandardItem(str(self.file_details["id"]))
-            model.setItem(i, 3, item)  # row, column, item (QStandardItem)
+                i = i + 1
 
-            i = i + 1
+                print self.file_details
+        except sjexc.StorjBridgeApiError as e:
+            # log the Storj Bridge Exception
+            return 0
 
-            print self.file_details
-
-        self.file_manager_ui.files_list_tableview.clearFocus()
-        self.file_manager_ui.files_list_tableview.setModel(model)
-        self.file_manager_ui.files_list_tableview.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
 
     def createNewBucketResolveThread(self):
         download_thread = threading.Thread(target=self.initialize_bucket_select_combobox, args=())
