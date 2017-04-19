937ff460a4d05d396aff1577162cf20d122d1dc2
Marco Rosa <marcor165@hotmail.it>
2017-04-19 11:44:52 +0200

    create logger

---
+++ UI/utilities/log_manager.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import sys
+
+
+LOGGER_NAME = 'storj-python-gui-log'
+LOG_FILENAME = 'test.log'
+PATH = os.path.join(os.getcwd(), LOG_FILENAME)
+
+logger = logging.getLogger(LOGGER_NAME)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+# Logs written to file
+# fileHandler = logging.FileHandler(PATH)
+# Rotating file logger. Max size 3 MB per 5 log files
+fileHandler = logging.handlers.RotatingFileHandler(PATH,
+                                                   maxBytes=(1048576 * 3),
+                                                   backupCount=5)
+fileHandler.setFormatter(formatter)
+# fileHandler.setLevel(logging.WARNING)
+fileHandler.setLevel(logging.DEBUG)  # TODO: change to WARNING
+logger.addHandler(fileHandler)
+
+# Logs to stdout
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+consoleHandler.setLevel(logging.DEBUG)
+logger.addHandler(consoleHandler)
