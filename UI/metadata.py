cc795a7a9bf570f71666be9a640da91b710d9488
Pedro Salgado <steenzout@users.noreply.github.com>
2017-04-27 09:21:27 -0600

    metadata.py: added.

---
+++ UI/metadata.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Storj GUI metadata module."""
+
+__author__ = ', '.join([
+    'Marco Rosa',
+    'Mike Bailey',
+    'Wiktor Jezioro'
+])
+__author_email__ = ';'.join([
+    'marcor165@hotmail.it',
+    'mibgranny@aol.com',
+    'lakewik@lakewik.pl'
+])
+# major version number should match the storj client major version
+__version__ = '0.3.0'
