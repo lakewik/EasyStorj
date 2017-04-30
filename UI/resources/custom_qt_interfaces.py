2e166a3c8fcc2eb41cb252fea0694b402dae7ed4
Wiktor Jezioro <wikjezioro@op.pl>
2017-04-30 20:13:42 +0200

    Create custom_qt_interfaces.py

---
+++ UI/resources/custom_qt_interfaces.py
@@ -0,0 +1,17 @@
+from PyQt4 import Qt
+from PyQt4 import QtCore, QtGui
+
+import operator
+from PyQt4.QtCore import QAbstractTableModel, SIGNAL
+from PyQt4.QtCore import QVariant
+
+class TableModel(QtGui.QStandardItemModel):
+    _sort_order = QtCore.Qt.AscendingOrder
+
+    def sortOrder(self):
+        return self._sort_order
+
+    def sort(self, column, order):
+        if column == 0:
+            self._sort_order = order
+            QtGui.QStandardItemModel.sort(self, column, order)
