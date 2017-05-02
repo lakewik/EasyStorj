8620a863ec0d9df6df331dc029084abedbd14ecf
Marco Rosa <marcor165@hotmail.it>
2017-05-02 23:40:10 +0200

    fix pep8 errors in bucket_manager.py

--- UI/bucket_manager.py
+++ UI/bucket_manager.py
@@ -24,20 +24,29 @@ class BucketManagerUI(QtGui.QMainWindow):
         self.bucket_manager_ui.setupUi(self)
         self.createNewBucketGetThread()
 
-        QtCore.QObject.connect(self.bucket_manager_ui.quit_bt, QtCore.SIGNAL('clicked()'),
-                               self.quit)  # open login window
-        QtCore.QObject.connect(self.bucket_manager_ui.delete_bucket_bt, QtCore.SIGNAL('clicked()'),
-                               self.delete_bucket)  # delete bucket
+        # Open login window
+        QtCore.QObject.connect(self.bucket_manager_ui.quit_bt,
+                               QtCore.SIGNAL('clicked()'),
+                               self.quit)
 
-        QtCore.QObject.connect(self.bucket_manager_ui.create_new_bucket_bt, QtCore.SIGNAL('clicked()'),
-                               self.open_bucket_create_window)  # open bucket create window
-        QtCore.QObject.connect(self.bucket_manager_ui.edit_bucket_bt, QtCore.SIGNAL('clicked()'),
-                               self.open_bucket_edit_window)  # open bucket edit window
-        # QtCore.QObject.connect(self.ui.pushButton_4, QtCore.SIGNAL('clicked()'),
-        # self.open_register_window) # open login window
+        # Delete bucket
+        QtCore.QObject.connect(self.bucket_manager_ui.delete_bucket_bt,
+                               QtCore.SIGNAL('clicked()'),
+                               self.delete_bucket)
+
+        # Open bucket create window
+        QtCore.QObject.connect(self.bucket_manager_ui.create_new_bucket_bt,
+                               QtCore.SIGNAL('clicked()'),
+                               self.open_bucket_create_window)
+
+        # Open bucket edit window
+        QtCore.QObject.connect(self.bucket_manager_ui.edit_bucket_bt,
+                               QtCore.SIGNAL('clicked()'),
+                               self.open_bucket_edit_window)
 
     def createNewBucketGetThread(self):
-        download_thread = threading.Thread(target=self.initialize_buckets_table, args=())
+        download_thread = threading.Thread(
+            target=self.initialize_buckets_table, args=())
         download_thread.start()
 
     def quit(self):
@@ -49,11 +58,13 @@ class BucketManagerUI(QtGui.QMainWindow):
         bucket_name = ''
 
         tablemodel = self.bucket_manager_ui.bucket_list_tableview.model()
-        rows = sorted(set(index.row() for index in self.bucket_manager_ui.bucket_list_tableview.selectedIndexes()))
+        rows = sorted(
+            set(index.row() for index in
+                self.bucket_manager_ui.bucket_list_tableview.selectedIndexes()))
         i = 0
         for row in rows:
-            index = tablemodel.index(row, 3)  # get bucket ID
-            index2 = tablemodel.index(row, 2)  # get bucket name
+            index = tablemodel.index(row, 3)  # Get bucket ID
+            index2 = tablemodel.index(row, 2)  # Get bucket name
             # We suppose data are strings
             bucket_id = str(tablemodel.data(index).toString())
             bucket_name = str(tablemodel.data(index2).toString())
@@ -64,8 +75,9 @@ class BucketManagerUI(QtGui.QMainWindow):
             msgBox = QtGui.QMessageBox(
                 QtGui.QMessageBox.Question,
                 'Are you sure?',
-                'Are you sure to delete this bucket? Bucket name: \'%s\'' % bucket_name,
-                (QtGui.QMessageBox.Yes | QtGui.QMessageBox.No))
+                'Are you sure to delete this bucket? Bucket name: \'%s\'' %
+                bucket_name,
+                QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
             result = msgBox.exec_()
             if result == QtGui.QMessageBox.Yes:
                 success = False
@@ -100,9 +112,10 @@ class BucketManagerUI(QtGui.QMainWindow):
         self.bucket_create_window.show()
 
     def initialize_buckets_table(self):
-        self.storj_engine = StorjEngine()  # init StorjEngine
+        self.storj_engine = StorjEngine()  # Init StorjEngine
         logger.info('resolving buckets')
-        model = QStandardItemModel(1, 1)  # initialize model for inserting to table
+        # Initialize model for inserting to table
+        model = QStandardItemModel(1, 1)
 
         model.setHorizontalHeaderLabels(['Name', 'Storage', 'Transfer', 'ID'])
 
@@ -129,6 +142,9 @@ class BucketManagerUI(QtGui.QMainWindow):
                 'Unhandled bucket resolving exception',
                 'Exception: %s' % e)
 
-        self.bucket_manager_ui.total_buckets_label.setText(str(i))  # set label of user buckets number
+        # Set label of user buckets number
+        self.bucket_manager_ui.total_buckets_label.setText(str(i))
         self.bucket_manager_ui.bucket_list_tableview.setModel(model)
-        self.bucket_manager_ui.bucket_list_tableview.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        self.bucket_manager_ui.bucket_list_tableview.horizontalHeader().\
+            setResizeMode(QtGui.QHeaderView.Stretch)
+
