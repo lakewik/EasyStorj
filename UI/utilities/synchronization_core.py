f513877108544734ebf315bc9a83c452d7e7a53c
Wiktor Jezioro <wikjezioro@op.pl>
2017-05-03 22:38:10 +0200

    Create synchronization core draft

---
+++ UI/utilities/synchronization_core.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Synchronization core module for Storj GUI Client #
+
+class StorjFileSynchronization():
+    def start_sync_thread(self):
+        return 1
+    def reload_sync_configuration(self):
+        return 1
+
+    def add_file_to_sync_queue(self):
+        return 1
