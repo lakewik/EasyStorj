8f5ccfad0a51af8feb9138d45350b79fb556d5ff
Wiktor Jezioro <wikjezioro@op.pl>
2017-05-03 21:48:46 +0200

    Add buttons handling

--- UI/client_config.py
+++ UI/client_config.py
@@ -2,6 +2,7 @@ from PyQt4 import QtCore, QtGui
 from .utilities.backend_config import Configuration
 from .qt_interfaces.settings_ui_new import Ui_ClientConfiguration
 from .utilities.log_manager import logger
+from .utilities.account_manager import AccountManager
 
 
 # Configuration Ui section
@@ -15,6 +16,7 @@ class ClientConfigurationUI(QtGui.QMainWindow):
         self.client_configuration_ui.setupUi(self)
 
         self.configuration_manager = Configuration()
+        self.account_manager = AccountManager()
 
         QtCore.QObject.connect(self.client_configuration_ui.apply_bt, QtCore.SIGNAL('clicked()'),
                                self.save_settings)  # save settings action
@@ -25,7 +27,35 @@ class ClientConfigurationUI(QtGui.QMainWindow):
         QtCore.QObject.connect(self.client_configuration_ui.crypto_keys_location_select_bt, QtCore.SIGNAL('clicked()'),
                                self.select_crypto_keys_path)  # open path select
 
+        QtCore.QObject.connect(self.client_configuration_ui.logout_bt, QtCore.SIGNAL('clicked()'),
+                               self.handle_logout_action)  # logout
+
+        QtCore.QObject.connect(self.client_configuration_ui.clear_logs_bt, QtCore.SIGNAL('clicked()'),
+                               self.handle_clear_logs_action)  # clear logs
+
+        QtCore.QObject.connect(self.client_configuration_ui.restore_to_default_bt, QtCore.SIGNAL('clicked()'),
+                               self.reset_settings_to_default)  # restore to default settings
+
+
+
+
         self.configuration_manager.paint_config_to_ui(self.client_configuration_ui)
+
+
+    def handle_logout_action(self):
+        msgBox = QtGui.QMessageBox(
+            QtGui.QMessageBox.Question,
+            'Question',
+            'Are you sure that you want to logout?',
+            (QtGui.QMessageBox.Yes | QtGui.QMessageBox.No))
+
+        result = msgBox.exec_()
+        #self.__logger.debug(result)
+
+        if result == QtGui.QMessageBox.Yes:
+            logged_out = self.account_manager.logout()
+            if logged_out:
+                QtGui.QMessageBox.about(self, 'Success', 'Successfully logged out!')
 
 
     def select_crypto_keys_path(self):
@@ -38,7 +68,27 @@ class ClientConfigurationUI(QtGui.QMainWindow):
         self.configuration_manager.save_client_configuration(self.client_configuration_ui)  # save configuration
         QtGui.QMessageBox.about(self, 'Success', 'Configuration saved successfully!')
 
+    def handle_clear_logs_action(self):
+        msgBox = QtGui.QMessageBox(
+            QtGui.QMessageBox.Question,
+            'Question',
+            'Are you sure that you want to clear all logs?',
+            (QtGui.QMessageBox.Yes | QtGui.QMessageBox.No))
+
+        result = msgBox.exec_()
+
+        if result == QtGui.QMessageBox.Yes:
+            QtGui.QMessageBox.about(self, 'Success', 'All logs have been successfully cleared!')
 
 
     def reset_settings_to_default(self):
+        msgBox = QtGui.QMessageBox(
+            QtGui.QMessageBox.Question,
+            'Question',
+            'Are you sure that you want to restore to default settings?',
+            (QtGui.QMessageBox.Yes | QtGui.QMessageBox.No))
+
+        result = msgBox.exec_()
+        if result == QtGui.QMessageBox.Yes:
+            QtGui.QMessageBox.about(self, 'Success', 'Settings successfully restored to default!')
         logger.debug(1)
