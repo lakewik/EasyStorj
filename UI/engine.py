af8361123295f8df5663d87c55f6e22bd0b734c2
Wiktor Jezioro <wikjezioro@op.pl>
2017-05-14 22:17:37 +0200

    Respect config

--- UI/engine.py
+++ UI/engine.py
@@ -1,26 +1,24 @@
-# -*- coding: utf-8 -*-
-
-import logging
+from utilities.account_manager import AccountManager
+from utilities.backend_config import Configuration
 import storj
-
-from .utilities.account_manager import AccountManager
+from utilities.log_manager import logger
 
 
 class StorjEngine:
 
-    __logger = logging.getLogger('%s.StorjEngine' % __name__)
-
     def __init__(self):
+        self.conf_manager = Configuration()
         self.account_manager = AccountManager()
         self.password = None
         if self.account_manager.if_logged_in():
             self.password = self.account_manager.get_user_password()
             self.email = self.account_manager.get_user_email()
-
             # initialize Storj
-            self.storj_client = storj.Client(
-                email=self.email,
-                password=self.password,
-                do_hashing=False, timeout=15)
-            self.__logger.debug('Login from credentials xml file')
-        self.__logger.debug('testlogin, StorjEngine')
+            max_bridge_request_timeout = self.conf_manager.get_max_bridge_request_timeout()
+            print "Using Bridge timeout: " + str(max_bridge_request_timeout)
+            self.storj_client = storj.Client(email=self.email,
+                                             password=self.password,
+                                             do_hashing=False,
+                                             timeout=max_bridge_request_timeout)
+            logger.debug("Login from credentials xml file")
+        logger.debug("testlogin, StorjEngine")
