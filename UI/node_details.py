824ccf08a6803ef84ea926a64867439c59f89b6d
Marco Rosa <marcor165@hotmail.it>
2017-06-08 23:31:00 +0200

    code refactoring

--- UI/node_details.py
+++ UI/node_details.py
@@ -2,11 +2,10 @@
 
 import json
 import logging
-import socket
-import threading
-
 import pycountry
+import socket
 import storj.exception as sjexc
+import threading
 
 from PyQt4 import QtCore, QtGui
 
@@ -32,31 +31,35 @@ class NodeDetailsUI(QtGui.QMainWindow):
         self.tools = Tools()
 
         # close window
-        QtCore.QObject.connect(self.node_details_ui.ok_bt, QtCore.SIGNAL('clicked()'), self.close)
-        self.connect(self, QtCore.SIGNAL('showBridgeExceptionMessageBox'), self.show_storj_bridge_exception)
+        QtCore.QObject.connect(self.node_details_ui.ok_bt,
+                               QtCore.SIGNAL('clicked()'), self.close)
+        self.connect(self, QtCore.SIGNAL('showBridgeExceptionMessageBox'),
+                     self.show_storj_bridge_exception)
 
         self.createNewNodeDetailsResolveThread()
 
     def show_storj_bridge_exception(self, exception_content):
         try:
-            j = json.loads(str(exception_content))
+            j = json.loads(exception_content)
             QtGui.QMessageBox.critical(self, 'Bridge error', str(j['error']))
         except Exception as e:
             self.__logger.error(e)
-            QtGui.QMessageBox.critical(self, 'Bridge error', str(exception_content))
+            QtGui.QMessageBox.critical(self, 'Bridge error', exception_content)
 
     def createNewNodeDetailsResolveThread(self):
-        download_thread = threading.Thread(target=self.initialize_node_details, args=())
+        download_thread = threading.Thread(target=self.initialize_node_details,
+                                           args=())
         download_thread.start()
 
     def initialize_node_details(self):
 
         try:
-            self.node_details_content = self.storj_engine.storj_client.contact_lookup(str(self.nodeid))
+            self.node_details_content = \
+                self.storj_engine.storj_client.contact_lookup(self.nodeid)
 
             # get given node address
             self.node_details_ui.address_label.setText(
-                str(self.node_details_content.address))
+                self.node_details_content.address)
             # get last timeout
             self.node_details_ui.last_timeout_label.setText(
                 str(self.node_details_content.lastTimeout))
@@ -65,23 +68,23 @@ class NodeDetailsUI(QtGui.QMainWindow):
                 str(self.node_details_content.timeoutRate))
             # get user agent
             self.node_details_ui.user_agent_label.setText(
-                str(self.node_details_content.userAgent))
+                self.node_details_content.userAgent)
             # get protocol version
             self.node_details_ui.protocol_version_label.setText(
                 str(self.node_details_content.protocol))
             # get farmer node response time
-            self.node_details_ui.response_time_label.setText(str(
-                self.node_details_content.responseTime))
+            self.node_details_ui.response_time_label.setText(
+                str(self.node_details_content.responseTime))
             # get farmer node port
             self.node_details_ui.port_label.setText(
                 str(self.node_details_content.port))
             # get farmer node response time
             self.node_details_ui.node_id_label.setText(
-                str(self.nodeid))
+                self.nodeid)
 
             # ping_to_node = self.tools.measure_ping_latency(str(self.node_details_content.address))
 
-            ip_addr = socket.gethostbyname(str(self.node_details_content.address))
+            ip_addr = socket.gethostbyname(self.node_details_content.address)
 
             obj = IPWhois(ip_addr)
             res = obj.lookup_whois()
@@ -92,7 +95,7 @@ class NodeDetailsUI(QtGui.QMainWindow):
             country_full_name = country_parsed.name
 
             self.node_details_ui.country_label.setText(
-                str(country_full_name))  # set full country name
+                country_full_name)  # Set full country name
 
             # ## Display country flag ###
 
