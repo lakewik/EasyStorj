006c1e28dad4a13b939cd0a0ac5d47c74e21310e
Marco Rosa <marcor165@hotmail.it>
2017-06-08 23:19:06 +0200

    do not use print. use loggers

--- UI/engine.py
+++ UI/engine.py
@@ -1,7 +1,7 @@
 from utilities.account_manager import AccountManager
 from utilities.backend_config import Configuration
-import storj
 import logging
+import storj
 
 
 class StorjEngine:
@@ -16,11 +16,13 @@ class StorjEngine:
             self.password = self.account_manager.get_user_password()
             self.email = self.account_manager.get_user_email()
             # initialize Storj
-            max_bridge_request_timeout = self.conf_manager.get_max_bridge_request_timeout()
-            print "Using Bridge timeout: " + str(max_bridge_request_timeout)
+            max_bridge_request_timeout = \
+                self.conf_manager.get_max_bridge_request_timeout()
+            self.__logger.info('Using Bridge timeout: %s' %
+                               max_bridge_request_timeout)
             self.storj_client = storj.Client(email=self.email,
                                              password=self.password,
                                              do_hashing=False,
                                              timeout=max_bridge_request_timeout)
-            self.__logger.debug("Login from credentials xml file")
-        self.__logger.debug("testlogin, StorjEngine")
+            self.__logger.debug('Login from credentials xml file')
+        self.__logger.debug('testlogin, StorjEngine')
