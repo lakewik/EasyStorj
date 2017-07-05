c68c20c501b051d874131cf23865f42cb01b9592
Marco Rosa <marcor165@hotmail.it>
2017-07-06 00:03:21 +0200

    code refactoring (pep8)

--- UI/utilities/tools.py
+++ UI/utilities/tools.py
@@ -6,7 +6,9 @@ from os.path import expanduser
 import tempfile
 import errno
 
+
 class Tools:
+
     def encrypt_file_name(self):
         return 1
 
@@ -31,14 +33,18 @@ class Tools:
         return True
 
     def check_email(self, email):
-        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+        if not re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)',
+                        email):
             return False
         else:
             return True
 
     def measure_ping_latency(self, destination_host):
-        ping_latency = str(os.system(
-            "ping " + ("-n 1 " if platform.system().lower() == "windows" else "-c 1 ") + str(destination_host)))
+        ping_latency = str(
+            os.system('ping ' +
+                      ('-n 1 ' if platform.system().lower() == 'windows'
+                       else '-c 1 ') +
+                      str(destination_host)))
 
         ping_data_parsed = pingparser.parse(ping_latency)
 
@@ -46,16 +52,20 @@ class Tools:
 
     def human_size(self, size_bytes):
         """
-        format a size in bytes into a 'human' file size, e.g. bytes, KB, MB, GB, TB, PB
-        Note that bytes/KB will be reported in whole numbers but MB and above will have greater precision
+        Format a size in bytes into a 'human' file size,
+        e.g. bytes, KB, MB, GB, TB, PB
+        Note that bytes/KB will be reported in whole numbers but MB and
+        above will have greater precision
         e.g. 1 byte, 43 bytes, 443 KB, 4.3 MB, 4.43 GB, etc
-        From: <http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size>
+        From: <http://stackoverflow.com/questions/1094841/reusable-library-
+        to-get-human-readable-version-of-file-size>
         """
         if size_bytes == 1:
             # because I really hate unnecessary plurals
-            return "1 byte"
+            return '1 Byte'
 
-        suffixes_table = [('bytes', 0), ('KB', 0), ('MB', 1), ('GB', 2), ('TB', 2), ('PB', 2)]
+        suffixes_table = [('bytes', 0), ('KB', 0), ('MB', 1),
+                          ('GB', 2), ('TB', 2), ('PB', 2)]
 
         num = float(size_bytes)
         for suffix, precision in suffixes_table:
@@ -64,18 +74,17 @@ class Tools:
             num /= 1024.0
 
         if precision == 0:
-            formatted_size = "%d" % num
+            formatted_size = '%d' % num
         else:
             formatted_size = str(round(num, ndigits=precision))
 
-        return "%s %s" % (formatted_size, suffix)
+        return '%s %s' % (formatted_size, suffix)
 
     def get_home_user_directory(self):
-        home = expanduser("~")
-        return str(home)
+        home = expanduser('~')
+        return home
 
     def count_directory_size(self, directory, include_subdirs):
-
         if include_subdirs:
             start_path = str(directory)
             total_size = 0
@@ -84,11 +93,12 @@ class Tools:
                     fp = os.path.join(dirpath, f)
                     total_size += os.path.getsize(fp)
         else:
-            total_size = sum(os.path.getsize(f) for f in os.listdir(str(directory)) if os.path.isfile(f))
+            total_size = sum(os.path.getsize(f) for f in
+                             os.listdir(str(directory)) if os.path.isfile(f))
 
         return total_size
 
     def count_files_in_dir(self, directory, include_subdirs=False):
-        files_count = len([name for name in os.listdir(str(directory)) if os.path.isfile(os.path.join(str(directory), name))])
+        files_count = len([name for name in os.listdir(str(directory)) if
+                           os.path.isfile(os.path.join(str(directory), name))])
         return files_count
-
