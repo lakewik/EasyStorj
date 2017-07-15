970d291ab6939de71c20bf9ac47929513659cc9a
Wiktor Jezioro <wikjezioro@op.pl>
2017-07-15 23:19:27 +0200

    Create OwnStorj node details module

---
+++ UI/flask_ownstorj/ownstorj/models/node.py
@@ -0,0 +1,43 @@
+from storj_gui_client.UI.utilities import account_manager
+from storj_gui_client.UI.engine import StorjEngine
+
+import socket
+from ipwhois import IPWhois
+import pycountry
+
+# Module for node details displaying for OwnStorj
+
+storj_engine = StorjEngine()  # init StorjEngine
+
+class OwnStorjNodeDetails:
+    def __init__(self):
+        self.node_details_content = None
+
+    def node_details(self, nodeID):
+        node_details_array = {}
+        self.node_details_content = storj_engine.storj_client.contact_lookup(str(nodeID))
+
+        node_details_array["address"] = self.node_details_content.address
+        node_details_array["lastTimeout"] = self.node_details_content.lastTimeout
+        node_details_array["timeoutRate"] = self.node_details_content.timeoutRate
+        node_details_array["userAgent"] = self.node_details_content.userAgent
+        node_details_array["protocol"] = self.node_details_content.protocol
+        node_details_array["responseTime"] = self.node_details_content.responseTime
+        node_details_array["port"] = self.node_details_content.port
+        node_details_array["nodeID"] = self.node_details_content.nodeID
+
+        ip_addr = socket.gethostbyname(str(self.node_details_content.address))
+
+        obj = IPWhois(ip_addr)
+        res = obj.lookup_whois()
+        country = res["nets"][0]['country']
+
+        country_parsed = pycountry.countries.get(alpha_2=str(country))
+
+        country_full_name = country_parsed.name
+
+        node_details_array["country_full_name"] = country_full_name
+        node_details_array["country_code"] = country
+
+        return node_details_array
+
